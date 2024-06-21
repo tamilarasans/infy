41de337baedfa14c8e15608369f571251093d036
tamilarasans <tamilarasans@users.noreply.github.com>
2024-06-22 00:53:45 +0530

    Update RewardAppExceptionHandler.java

--- rewardapp/src/main/java/com/chc/rewardapp/exceptions/RewardAppExceptionHandler.java
+++ rewardapp/src/main/java/com/chc/rewardapp/exceptions/RewardAppExceptionHandler.java
@@ -1,16 +1,28 @@
 package com.chc.rewardapp.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+
 
 @RestControllerAdvice
 public class RewardAppExceptionHandler {
+	Logger logger= LoggerFactory.getLogger(RewardAppExceptionHandler.class);
 	@ExceptionHandler(IllegalArgumentException.class)
 	public ResponseEntity<Object> handleException(IllegalArgumentException e) {
-		
-		return new ResponseEntity<Object>("Illegal arg exp from Global",HttpStatus.BAD_REQUEST);
+		 logger.error("IllegalArgumentException: {}", e.getMessage());
+		return new ResponseEntity<Object>("Invalid Input:",HttpStatus.BAD_REQUEST);
 	}
 
+	
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception ex, WebRequest request) {
+        logger.error("Exception: {}", ex.getMessage());
+        return new ResponseEntity<>("An error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
