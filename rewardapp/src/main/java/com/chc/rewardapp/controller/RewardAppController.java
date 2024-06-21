f21ee76b13d0132ec00a34ff4d3fcd8faa6e0cb9
tamilarasans <tamilarasans@users.noreply.github.com>
2024-06-22 00:47:34 +0530

    Update RewardAppController.java

--- rewardapp/src/main/java/com/chc/rewardapp/controller/RewardAppController.java
+++ rewardapp/src/main/java/com/chc/rewardapp/controller/RewardAppController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +12,29 @@ import org.springframework.web.bind.annotation.RestController;
 import com.chc.rewardapp.model.CustomerRewards;
 import com.chc.rewardapp.service.RewardAppService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/api/rewardpoints")
 public class RewardAppController {
 	Logger logger= LoggerFactory.getLogger(RewardAppController.class);
-	@Autowired
+	
 	private RewardAppService rewardAppService;
 	
-	@RequestMapping("/get/{customerId}")
-	public ResponseEntity<CustomerRewards> getCustomerRewardsPointsById(@PathVariable Integer customerId) throws Exception {
-		logger.info("Get customer details "+customerId.intValue());
+	RewardAppController(RewardAppService rewardAppService){
+		this.rewardAppService=rewardAppService;
+		
+	}
+	
+	@GetMapping("/get/{customerId}")
+	public ResponseEntity<CustomerRewards> getCustomerRewardsPointsById(@Valid @PathVariable Integer customerId) throws Exception {
+		if(customerId==null || customerId <=0) {
+			throw new IllegalArgumentException("Customer ID must be positive number");
+		}
+		logger.info("Get customer details ",customerId);
 		CustomerRewards customerRewards = rewardAppService.getCustomerRewardsPointsById(customerId);
 		return ResponseEntity.ok().body(customerRewards);
 	}
 
 }
+
