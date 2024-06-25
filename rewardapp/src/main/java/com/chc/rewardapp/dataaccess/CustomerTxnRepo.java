95d04c997237063ac57174ed9d221ac53c9c2baf
Tamil <tamilarasanslnn@gmail.com>
2024-06-25 17:46:04 +0530

    Feedback updates

--- rewardapp/src/main/java/com/chc/rewardapp/dataaccess/CustomerTxnRepo.java
+++ rewardapp/src/main/java/com/chc/rewardapp/dataaccess/CustomerTxnRepo.java
@@ -2,13 +2,12 @@ package com.chc.rewardapp.dataaccess;
 
 import java.util.List;
 
-
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.chc.rewardapp.model.CustomerTxns;
 
 @Repository
-public interface CustomerTxnRepo extends CrudRepository <CustomerTxns, Long>{
+public interface CustomerTxnRepo extends CrudRepository<CustomerTxns, Long> {
 	public List<CustomerTxns> findByCustomerIdOrderByPurchaseDateDesc(Integer customerId);
 }
