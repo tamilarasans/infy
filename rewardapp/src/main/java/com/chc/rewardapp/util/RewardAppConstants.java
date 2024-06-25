87d5284a9adb25ed67bc25f8d486214afbc0f6b1
Tamil <tamilarasanslnn@gmail.com>
2024-06-25 17:48:48 +0530

    Feedback updates

--- rewardapp/src/main/java/com/chc/rewardapp/util/RewardAppConstants.java
+++ rewardapp/src/main/java/com/chc/rewardapp/util/RewardAppConstants.java
@@ -2,13 +2,12 @@ package com.chc.rewardapp.util;
 
 public class RewardAppConstants {
 
-	public static final int REWARD_RANGE_RULE_1 = 50;
-	
-    
-    public static final int REWARD_POINTS_RULE_1 = 1;
-   
-    public static final int REWARD_RANGE_RULE_2 = 100;
-   
-    public static final int REWARD_POINTS_RULE_2 = 2;
-	
+	public static final int minTxnAmount = 50;
+
+	public static final int minRewardPoints = 1;
+
+	public static final int maxTxnAmount = 100;
+
+	public static final int maxRewardPoints = 2;
+
 }
